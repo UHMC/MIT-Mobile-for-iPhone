80dcae4ad78b4b12c44ca2be49a5da5b4a6f13de
Blake Skinner <bskinner@mit.edu>
2012-03-16 15:25:14 -0400

    Rip out the existing search code and use the new LocationSearchController instead.

--- Modules/Facilities/FacilitiesLocation/FacilitiesCategoryViewController.h
+++ Modules/Facilities/FacilitiesLocation/FacilitiesCategoryViewController.h
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
+#import "LocationSearchDelegate.h"
 
 @class FacilitiesLocation;
 @class FacilitiesCategory;
@@ -7,39 +8,9 @@
 @class HighlightTableViewCell;
 @class FacilitiesLocationSearch;
 
-@interface FacilitiesCategoryViewController : UIViewController <UITableViewDataSource,UITableViewDelegate,UISearchDisplayDelegate,UISearchBarDelegate> {
-    UITableView *_tableView;
-    MITLoadingActivityView *_loadingView;
-    
-    FacilitiesLocationData *_locationData;
-    FacilitiesLocationSearch *_searchHelper;
-    NSArray *_cachedData;
-    NSArray *_filteredData;
-    NSPredicate *_filterPredicate;
-    NSString *_searchString;
-    NSString *_trimmedString;
-}
-
+@interface FacilitiesCategoryViewController : UIViewController <UITableViewDataSource,UITableViewDelegate,LocationSearchDelegate>
 @property (nonatomic,retain) UITableView* tableView;
 @property (nonatomic,retain) MITLoadingActivityView* loadingView;
 @property (retain) FacilitiesLocationData* locationData;
-@property (nonatomic,retain) NSPredicate* filterPredicate;
-
-@property (nonatomic,retain) NSArray* cachedData;
-@property (nonatomic,retain) NSArray* filteredData;
-@property (nonatomic,retain) NSString* searchString;
-@property (nonatomic,retain) NSString *trimmedString;
-
-- (NSArray*)dataForMainTableView;
-- (NSArray*)resultsForSearchString:(NSString*)searchText;
-
-- (void)configureMainTableCell:(UITableViewCell*)cell forIndexPath:(NSIndexPath*)indexPath;
-- (void)configureSearchCell:(HighlightTableViewCell*)cell forIndexPath:(NSIndexPath*)indexPath;
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText;
-- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar;
 
 @end
--- Modules/Facilities/FacilitiesLocation/FacilitiesCategoryViewController.m
+++ Modules/Facilities/FacilitiesLocation/FacilitiesCategoryViewController.m
@@ -13,28 +13,24 @@
 #import "MITLoadingActivityView.h"
 #import "UIKit+MITAdditions.h"
 #import "FacilitiesLocationSearch.h"
+#import "LocationSearchController.h"
 
 
 @interface FacilitiesCategoryViewController ()
-@property (nonatomic,retain) FacilitiesLocationSearch *searchHelper;
+@property (nonatomic,strong) LocationSearchController *locationSearchController;
+@property (nonatomic,strong) NSArray *cachedData;
+
 - (BOOL)shouldShowLocationSection;
 - (NSArray*)dataForMainTableView;
 - (void)configureMainTableCell:(UITableViewCell*)cell forIndexPath:(NSIndexPath*)indexPath;
-- (NSArray*)resultsForSearchString:(NSString*)searchText;
-- (void)configureSearchCell:(HighlightTableViewCell*)cell forIndexPath:(NSIndexPath*)indexPath;
 @end
 
 @implementation FacilitiesCategoryViewController
 @synthesize tableView = _tableView;
 @synthesize loadingView = _loadingView;
 @synthesize locationData = _locationData;
-@synthesize searchString = _searchString;
-@synthesize trimmedString = _trimmedString;
-@synthesize searchHelper = _searchHelper;
-
-@dynamic cachedData;
-@dynamic filteredData;
-@dynamic filterPredicate;
+@synthesize cachedData = _cachedData;
+@synthesize locationSearchController = _locationSearchController;
 
 - (id)init
 {
@@ -42,7 +38,6 @@
     if (self) {
         self.title = @"Where is it?";
         self.locationData = [FacilitiesLocationData sharedData];
-        self.filterPredicate = [NSPredicate predicateWithFormat:@"locations.@count > 0"];
     }
     return self;
 }
@@ -51,13 +46,10 @@
 {
     self.tableView = nil;
     self.cachedData = nil;
-	self.filteredData = nil;
 	self.locationData = nil;
     self.loadingView = nil;
-    self.searchString = nil;
-    self.filterPredicate = nil;
     self.cachedData = nil;
-    self.filteredData = nil;
+    self.locationSearchController = nil;
     [super dealloc];
 }
 
@@ -80,19 +72,14 @@
     CGRect searchBarFrame = CGRectZero;
     
     {
-        UISearchBar *searchBar = [[[UISearchBar alloc] init] autorelease];
-        searchBar.delegate = self;
-        searchBar.barStyle = UIBarStyleBlackOpaque;
+        LocationSearchController *controller = [[[LocationSearchController alloc] initWithContentsController:self] autorelease];
+        controller.resultDelegate = self;
+        controller.searchBar.barStyle = UIBarStyleBlackOpaque;
+        self.locationSearchController = controller;
         
-        UISearchDisplayController *searchController = [[UISearchDisplayController alloc] initWithSearchBar:searchBar
-                                                                                         contentsController:self];
-        searchController.delegate = self;
-        searchController.searchResultsDataSource = self;
-        searchController.searchResultsDelegate = self;
-        
-        [searchBar sizeToFit];
-        searchBarFrame = searchBar.frame;
-        [mainView addSubview:searchBar];
+        [controller.searchBar sizeToFit];
+        searchBarFrame = controller.searchBar.frame;
+        [mainView addSubview:controller.searchBar];
     }
     
     {
@@ -169,8 +156,7 @@
                                      [self.tableView reloadData];
                                  }
                              } else if ([userData isEqualToString:FacilitiesLocationsKey]) {
-                                 if ([self.searchDisplayController isActive] && ((self.filteredData == nil) || updated)) {
-                                     self.filteredData = nil;
+                                 if ([self.searchDisplayController isActive] && updated) {
                                      [self.searchDisplayController.searchResultsTableView reloadData];
                                  }
                              }
@@ -212,28 +198,6 @@
     return data;
 }
 
-- (NSArray*)resultsForSearchString:(NSString *)searchText {
-    if (self.searchHelper == nil) {
-        self.searchHelper = [[[FacilitiesLocationSearch alloc] init] autorelease];
-    }
-    
-    self.searchHelper.category = nil;
-    self.searchHelper.searchString = searchText;
-    NSArray *results = [self.searchHelper searchResults];
-    
-    results = [results sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
-        NSString *key1 = [obj1 valueForKey:FacilitiesSearchResultDisplayStringKey];
-        NSString *key2 = [obj2 valueForKey:FacilitiesSearchResultDisplayStringKey];
-
-        return [key1 compare:key2
-              options:(NSCaseInsensitiveSearch |
-                       NSNumericSearch |
-                       NSForcedOrderingSearch)];
-    }];
-    
-    return results;
-}
-
 - (void)configureMainTableCell:(UITableViewCell *)cell
                   forIndexPath:(NSIndexPath *)indexPath
 {
@@ -245,91 +209,32 @@
     }
 }
 
-- (void)configureSearchCell:(HighlightTableViewCell *)cell
-                forIndexPath:(NSIndexPath *)indexPath
-{
-    NSDictionary *loc = [self.filteredData objectAtIndex:indexPath.row];
-    
-    cell.highlightLabel.searchString = self.searchString;
-    cell.highlightLabel.text = [loc objectForKey:FacilitiesSearchResultDisplayStringKey];
-}
-
 
 #pragma mark - Dynamic Setters/Getters
-- (void)setFilterPredicate:(NSPredicate *)filterPredicate {
-    self.cachedData = nil;
-    [_filterPredicate release];
-    _filterPredicate = [filterPredicate retain];
-}
-
-- (NSPredicate*)filterPredicate {
-    return _filterPredicate;
-}
-
 - (void)setCachedData:(NSArray *)cachedData {
-    if (_cachedData != nil) {
-        [_cachedData release];
-    }
-    
+    [_cachedData release];
     _cachedData = [cachedData retain];
 }
 
 - (NSArray*)cachedData {
     if (_cachedData == nil) {
-        [self setCachedData:[self dataForMainTableView]];
+        self.cachedData = [self dataForMainTableView];
     }
     
     return _cachedData;
 }
 
-- (void)setFilteredData:(NSArray *)filteredData {
-    [_filteredData release];
-    _filteredData = [filteredData retain];
-}
-
-- (NSArray*)filteredData {
-    if (_filteredData == nil && [self.searchString length] > 0) {
-        [self setFilteredData:[self resultsForSearchString:self.searchString]];
-    }
-    
-    return _filteredData;
-}
-
-
 #pragma mark - UITableViewDelegate Methods
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UIViewController *nextViewController = nil;
     
-    if (tableView == self.tableView) {
-        if ((indexPath.section == 0) && [self shouldShowLocationSection]) {
-            nextViewController = [[[FacilitiesUserLocationViewController alloc] init] autorelease];
-        } else {
-            FacilitiesCategory *category = (FacilitiesCategory*)[self.cachedData objectAtIndex:indexPath.row];
-            FacilitiesLocationViewController *controller = [[[FacilitiesLocationViewController alloc] init] autorelease];
-            controller.category = category;
-            nextViewController = controller;
-        }
+    if ((indexPath.section == 0) && [self shouldShowLocationSection]) {
+        nextViewController = [[[FacilitiesUserLocationViewController alloc] init] autorelease];
     } else {
-        if (indexPath.row == 0) {
-            FacilitiesTypeViewController *vc = [[[FacilitiesTypeViewController alloc] init] autorelease];
-            vc.userData = [NSDictionary dictionaryWithObject: self.searchString
-                                                      forKey: FacilitiesRequestLocationUserBuildingKey];
-            nextViewController = vc;
-        } else {
-            
-            NSDictionary *dict = [self.filteredData objectAtIndex:indexPath.row-1];
-            FacilitiesLocation *location = (FacilitiesLocation*)[dict objectForKey:FacilitiesSearchResultLocationKey];
-            
-            if ([location.isLeased boolValue]) {
-                FacilitiesLeasedViewController *controller = [[[FacilitiesLeasedViewController alloc] initWithLocation:location] autorelease];
-                
-                nextViewController = controller;
-            } else {
-                FacilitiesRoomViewController *controller = [[[FacilitiesRoomViewController alloc] init] autorelease];
-                controller.location = location;
-                nextViewController = controller;
-            }
-        }
+        FacilitiesCategory *category = (FacilitiesCategory*)[self.cachedData objectAtIndex:indexPath.row];
+        FacilitiesLocationViewController *controller = [[[FacilitiesLocationViewController alloc] init] autorelease];
+        controller.category = category;
+        nextViewController = controller;
     }
     
     [self.navigationController pushViewController:nextViewController
@@ -341,91 +246,61 @@
 
 #pragma mark - UITableViewDataSource Methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    if (tableView == self.tableView) {
-        return ([self shouldShowLocationSection] ? 2 : 1);
-    } else {
-        return 1;
-    }
+    return ([self shouldShowLocationSection] ? 2 : 1);
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (tableView == self.tableView) {
-        return ((section == 0) && [self shouldShowLocationSection]) ? 1 : [self.cachedData count];
-    } else {
-        return ([self.trimmedString length] > 0) ? [self.filteredData count] + 1 : 0;
-    }
+    return ((section == 0) && [self shouldShowLocationSection]) ? 1 : [self.cachedData count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *facilitiesIdentifier = @"facilitiesCell";
-    static NSString *searchIdentifier = @"searchCell";
     
-    if (tableView == self.tableView) {
-        UITableViewCell *cell = nil;
-        cell = [tableView dequeueReusableCellWithIdentifier:facilitiesIdentifier];
-        
-        if (cell == nil) {
-            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                           reuseIdentifier:facilitiesIdentifier] autorelease];
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        }
-        
-        [self configureMainTableCell:cell 
-                        forIndexPath:indexPath];
-        return cell;
-    } else if (tableView == self.searchDisplayController.searchResultsTableView) {
-        HighlightTableViewCell *hlCell = nil;
-        hlCell = (HighlightTableViewCell*)[tableView dequeueReusableCellWithIdentifier:searchIdentifier];
-        
-        if (hlCell == nil) {
-            hlCell = [[[HighlightTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                                    reuseIdentifier:searchIdentifier] autorelease];
-            
-            hlCell.autoresizesSubviews = YES;
-            hlCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        }
-        
-        if (indexPath.row == 0) {
-            hlCell.highlightLabel.searchString = nil;
-            hlCell.highlightLabel.text = [NSString stringWithFormat:@"Use \"%@\"",self.searchString];
-        } else {
-            NSIndexPath *path = [NSIndexPath indexPathForRow:(indexPath.row-1)
-                                                   inSection:indexPath.section];
-            [self configureSearchCell:hlCell
-                         forIndexPath:path];
-        }
-        
-        
-        return hlCell;
-    } else {
-        return nil;
+    UITableViewCell *cell = nil;
+    cell = [tableView dequeueReusableCellWithIdentifier:facilitiesIdentifier];
+    
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                       reuseIdentifier:facilitiesIdentifier] autorelease];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
+    
+    [self configureMainTableCell:cell 
+                    forIndexPath:indexPath];
+    return cell;
 }
 
-#pragma mark - UISearchBarDelegate
-- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-    self.trimmedString = [searchText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    if (![self.searchString isEqualToString:self.trimmedString]) {
-        self.searchString = ([self.trimmedString length] > 0) ? self.trimmedString : nil;
-        self.filteredData = nil;
+#pragma mark - LocationSearchDelelgate
+- (void)locationSearch:(LocationSearchController*)controller didFailWithError:(NSError*)error
+{
+    
+}
+
+- (void)locationSearch:(LocationSearchController*)controller didFinishWithSearchString:(NSString*)string
+{
+    FacilitiesTypeViewController *vc = [[[FacilitiesTypeViewController alloc] init] autorelease];
+    vc.userData = [NSDictionary dictionaryWithObject: string
+                                              forKey: FacilitiesRequestLocationUserBuildingKey];
+    [self.navigationController pushViewController:vc
+                                         animated:YES];
+}
+
+- (void)locationSearch:(LocationSearchController*)controller didFinishWithResult:(FacilitiesLocation*)location
+{
+    UIViewController *nextViewController = nil;
+    if ([location.isLeased boolValue])
+    {
+        FacilitiesLeasedViewController *controller = [[[FacilitiesLeasedViewController alloc] initWithLocation:location] autorelease];
+        nextViewController = controller;
     }
+    else
+    {
+        FacilitiesRoomViewController *controller = [[[FacilitiesRoomViewController alloc] init] autorelease];
+        controller.location = location;
+        nextViewController = controller;
+    }
+    
+    [self.navigationController pushViewController:nextViewController
+                                         animated:YES];
 }
-
-- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
-    [self.searchDisplayController setActive:NO
-                                   animated:YES];
-}
-
-// Make sure tapping the status bar always scrolls to the top of the active table
-- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView {
-    self.tableView.scrollsToTop = NO;
-    tableView.scrollsToTop = YES;
-}
-
-- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView {
-    // using willUnload because willHide strangely doesn't get called when the "Cancel" button is clicked
-    tableView.scrollsToTop = NO;
-    self.tableView.scrollsToTop = YES;
-}
-
 @end
