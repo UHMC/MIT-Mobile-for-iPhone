bacb30c5b50570828fb7bba653c6cafb0170ea66
Blake Skinner <bskinner@mit.edu>
2012-03-16 15:23:25 -0400

    Create a stand-along search object for searching locations. Replaces the search code used in the various Facilities* classes and adds support for optionally using free-text entry and recent searches

--- Modules/Facilities/Location Search/LocationSearchController.h
+++ Modules/Facilities/Location Search/LocationSearchController.h
@@ -1,8 +1,14 @@
 #import <UIKit/UIKit.h>
 #import "LocationSearchDelegate.h"
 
-@interface LocationSearchController : UITableViewController <UISearchBarDelegate, UISearchDisplayDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface LocationSearchController : NSObject <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate>
+@property (nonatomic,readonly,strong) UIViewController *contentsController;
+@property (nonatomic,readonly,strong) UISearchBar *searchBar;
 @property (nonatomic,assign) id<LocationSearchDelegate> resultDelegate;
-- (id)init;
 
+@property (nonatomic,assign) BOOL allowsFreeTextEntry;
+@property (nonatomic,assign) BOOL showRecentSearches;
+
+- (id)initWithContentsController:(UIViewController*)contentsController;
+- (void)clearRecentSearches;
 @end
--- Modules/Facilities/Location Search/LocationSearchController.m
+++ Modules/Facilities/Location Search/LocationSearchController.m
@@ -3,28 +3,70 @@
 #import "CoreDataManager.h"
 #import "FacilitiesLocationSearch.h"
 #import "Foundation+MITAdditions.h"
+#import "HighlightTableViewCell.h"
 
 enum {
-    LocationSearchSectionAsEntered,
+    LocationSearchSectionAsEntered = 0,
     LocationSearchSectionRecent,
     LocationSearchSectionQuery
 };
 
 @interface LocationSearchController ()
+@property (nonatomic,strong) UISearchDisplayController *searchDisplayController;
+@property (nonatomic,strong) UIViewController *contentsController;
+@property (nonatomic,strong) UISearchBar *searchBar;
 @property (nonatomic,strong) NSString *searchString;
 @property (nonatomic,strong) NSArray *queryResults;
+
+@property (nonatomic,strong) NSMutableArray *recentSearches;
+@property (nonatomic,strong) NSArray *filteredRecentSearches;
+
+@property (nonatomic,strong) FacilitiesLocationSearch *searchHelper;
+@property (nonatomic,strong) NSMutableIndexSet *visibleSections;
+
+- (void)addStringToRecentSearches:(NSString*)string;
 @end
 
 @implementation LocationSearchController
 @synthesize resultDelegate = _resultDelegate;
 @synthesize queryResults = _queryResults;
+@synthesize recentSearches = _allRecentSearches;
+@synthesize searchHelper = _searchHelper;
+@synthesize visibleSections = _visibleSections;
+@synthesize filteredRecentSearches = _filteredRecentSearches;
+@synthesize searchString = _searchString;
+@synthesize searchDisplayController = _searchDisplayController;
+@synthesize contentsController = _contentsController;
+@synthesize searchBar = _searchBar;
 
-- (id)init
+@dynamic allowsFreeTextEntry;
+@dynamic showRecentSearches;
+
+- (id)initWithContentsController:(UIViewController *)contentsController
 {
-    self = [super initWithStyle:UITableViewStylePlain];
+    self = [super init];
     if (self)
     {
+        self.recentSearches = [NSMutableArray arrayWithArray:[[NSUserDefaults standardUserDefaults] arrayForKey:@"edu.mit.mobile.location.Recent"]];
         
+        self.visibleSections = [NSMutableIndexSet indexSet];
+        [self.visibleSections addIndex:LocationSearchSectionAsEntered];
+        [self.visibleSections addIndex:LocationSearchSectionRecent];
+        [self.visibleSections addIndex:LocationSearchSectionQuery];
+        
+        self.allowsFreeTextEntry = NO;
+        self.showRecentSearches = YES;
+        
+        UISearchBar *searchBar = [[[UISearchBar alloc] init] autorelease];
+        searchBar.delegate = self;
+        self.searchBar = searchBar;
+        
+        UISearchDisplayController *searchController = [[[UISearchDisplayController alloc] initWithSearchBar:searchBar
+                                                                                         contentsController:contentsController] autorelease];
+        searchController.delegate = self;
+        searchController.searchResultsDelegate = self;
+        searchController.searchResultsDataSource = self;
+        self.searchDisplayController = searchController;
     }
     
     return self;
@@ -32,51 +74,196 @@ enum {
 
 - (void)dealloc
 {
-    self.searchString = nil;
+    self.resultDelegate = nil;
     self.queryResults = nil;
+    self.recentSearches = nil;
+    self.searchHelper = nil;
+    self.visibleSections = nil;
+    self.filteredRecentSearches = nil;
+    self.searchString = nil;
     [super dealloc];
+}
+
+#pragma mark - Property Methods
+- (NSArray*)queryResults
+{
+    if (_queryResults == nil)
+    {
+        self.queryResults = [self locationResultsForQuery:self.searchString];
+    }
+    
+    return _queryResults;
+}
+
+- (NSArray*)filteredRecentSearches
+{
+    if (_filteredRecentSearches == nil)
+    {
+        self.filteredRecentSearches = [self recentSearchesForQuery:self.searchString];
+    }
+    
+    return _filteredRecentSearches;
+}
+
+- (void)setAllowsFreeTextEntry:(BOOL)allowsFreeTextEntry
+{
+    if (allowsFreeTextEntry)
+    {
+        [self.visibleSections addIndex:LocationSearchSectionAsEntered];
+    }
+    else
+    {
+        [self.visibleSections removeIndex:LocationSearchSectionAsEntered];
+    }
+    
+    [self.searchDisplayController.searchResultsTableView reloadData];
+}
+
+- (BOOL)allowsFreeTextEntry
+{
+    return [self.visibleSections containsIndex:LocationSearchSectionAsEntered];
+}
+
+- (void)setShowRecentSearches:(BOOL)showRecentSearches
+{
+    if (showRecentSearches)
+    {
+        [self.visibleSections addIndex:LocationSearchSectionRecent];
+    }
+    else
+    {
+        [self.visibleSections removeIndex:LocationSearchSectionRecent];
+    }
+    
+    [self.searchDisplayController.searchResultsTableView reloadData];
+}
+
+- (BOOL)showRecentSearches
+{
+    return [self.visibleSections containsIndex:LocationSearchSectionRecent];
 }
 
 #pragma mark - Private Methods
 - (NSArray*)recentSearchesForQuery:(NSString*)query
 {
-    NSArray *recentResults = [[NSUserDefaults standardUserDefaults] arrayForKey:@"edu.mit.mobile.location.Recent"];
-    NSMutableArray *filters = [NSMutableArray array];
+    NSMutableArray *filtered = [NSMutableArray array];
+    NSString *upperQuery = [query uppercaseString];
     
-    [recentResults enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        NSString *result = obj;
+    NSArray* words = [upperQuery componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    upperQuery = [words componentsJoinedByString:@""];
+    
+    [self.recentSearches enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSString *result = [(NSString*)obj uppercaseString];
+        NSArray* words = [result componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        result = [words componentsJoinedByString:@""];
         
-        
+        BOOL prefixMatch = (([upperQuery length] == 0) || [result hasPrefix:upperQuery]);
+        BOOL stringMatch = [result isEqualToString:upperQuery];
+        if (prefixMatch && (stringMatch == NO))
+        {
+            [filtered addObject:obj];
+        }
     }];
+    
+    return filtered;
 }
 
 - (NSArray*)locationResultsForQuery:(NSString*)query
 {
+    if (self.searchHelper == nil) {
+        self.searchHelper = [[[FacilitiesLocationSearch alloc] init] autorelease];
+    }
     
+    self.searchHelper.category = nil;
+    self.searchHelper.searchString = query;
+    NSArray *results = [self.searchHelper searchResults];
+    
+    NSLog(@"Found %d results from 'locations'", [results count]);
+    
+    results = [results sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        NSString *key1 = [obj1 valueForKey:FacilitiesSearchResultDisplayStringKey];
+        NSString *key2 = [obj2 valueForKey:FacilitiesSearchResultDisplayStringKey];
+        
+        return [key1 compare:key2
+                     options:(NSCaseInsensitiveSearch |
+                              NSNumericSearch |
+                              NSForcedOrderingSearch)];
+    }];
+    
+    return results;
+}
+
+- (NSUInteger)sectionForTableViewSection:(NSInteger)tableSection
+{
+    __block NSUInteger count = tableSection;
+    __block NSUInteger index = NSNotFound;
+    
+    [self.visibleSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
+        if (count == 0)
+        {
+            index = idx;
+            (*stop) = YES;
+        }
+        else
+        {
+            --count;
+        }
+    }];
+    
+    return index;
+}
+
+- (void)addStringToRecentSearches:(NSString *)string
+{
+    if ([self.recentSearches containsObject:string] == NO)
+    {
+        self.filteredRecentSearches = nil;
+        [self.recentSearches addObject:string];
+        [[NSUserDefaults standardUserDefaults] setObject:self.recentSearches
+                                                  forKey:@"edu.mit.mobile.location.Recent"];
+    }
+}
+
+- (void)clearRecentSearches
+{
+    self.filteredRecentSearches = nil;
+    self.recentSearches = nil;
+    [[NSUserDefaults standardUserDefaults] setObject:self.recentSearches
+                                              forKey:@"edu.mit.mobile.location.Recent"];
 }
 
 #pragma mark - UITableViewDelegate Methods
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    switch (indexPath.section)
+    NSUInteger section = [self sectionForTableViewSection:indexPath.section];
+    
+    switch (section)
     {
         case LocationSearchSectionAsEntered:
         {
+            [self addStringToRecentSearches:self.searchString];
+            
             if ([self.resultDelegate respondsToSelector:@selector(locationSearch:didFinishWithSearchString:)])
             {
                 [self.resultDelegate locationSearch:self
                           didFinishWithSearchString:self.searchString];
             }
+            
+            [self.searchDisplayController setActive:NO
+                                           animated:YES];
             break;
         }
             
         case LocationSearchSectionRecent:
         {
-            
+            self.searchString = [self.filteredRecentSearches objectAtIndex:indexPath.row];
+            self.searchBar.text = self.searchString;
             break;
         }
             
         case LocationSearchSectionQuery:
         {
+            [self addStringToRecentSearches:self.searchString];
+            
             if ([self.resultDelegate respondsToSelector:@selector(locationSearch:didFinishWithResult:)])
             {
                 NSDictionary *dict = [self.queryResults objectAtIndex:indexPath.row];
@@ -84,80 +271,159 @@ enum {
                 [self.resultDelegate locationSearch:self
                                 didFinishWithResult:location];
             }
+            
+            [self.searchDisplayController setActive:NO
+                                           animated:YES];
             break;
         }
         
         default:
             break;
     }
+    
+    [tableView deselectRowAtIndexPath:indexPath
+                             animated:YES];
 }
 
 #pragma mark - UITableViewDataSource Methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // The query se
-    NSUInteger sectionCount = 1;
+    NSInteger sectionCount = [self.visibleSections count];
+    return sectionCount;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (tableView == self.tableView) {
-        return ((section == 0) && [self shouldShowLocationSection]) ? 1 : [self.cachedData count];
-    } else {
-        return ([self.trimmedString length] > 0) ? [self.filteredData count] + 1 : 0;
+    NSUInteger index = [self sectionForTableViewSection:section];
+    NSInteger count = 0;
+    
+    switch (index)
+    {
+        case LocationSearchSectionAsEntered:
+            count = ([self.searchString length] > 0) ? 1 : 0;
+            break;
+            
+        case LocationSearchSectionRecent:
+            count = [self.filteredRecentSearches count];
+            break;
+            
+        case LocationSearchSectionQuery:
+            count = [self.queryResults count];
+            break;
     }
+    
+    NSLog(@"%d rows in section %d", count, index);
+    return count;
+}
+
+- (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSUInteger index = [self sectionForTableViewSection:section];
+    NSString *titleString = nil;
+    
+    switch (index)
+    {
+        case LocationSearchSectionAsEntered:
+        {
+            titleString = ([self.searchString length] > 0) ? @"Use As Entered" : nil;
+            break;
+        }
+            
+        case LocationSearchSectionRecent:
+        {
+            titleString = ([self.filteredRecentSearches count] > 0) ? @"Recent Searches" : nil;
+            break;
+        }
+            
+        case LocationSearchSectionQuery:
+        {
+            titleString = ([self.queryResults count] > 0) ? @"Search Results" : nil;
+            break;
+        }
+    }
+    
+    return titleString;
+}
+
+- (NSString*)cellIdentifierForSection:(NSUInteger)index
+{
+    switch(index)
+    {
+        case LocationSearchSectionAsEntered:
+            return @"edu.mit.mobile.location.asEnteredCell";
+            
+        case LocationSearchSectionRecent:
+            return @"edu.mit.mobile.location.recentSearchesCell";
+            
+        case LocationSearchSectionQuery:
+            return @"edu.mit.mobile.location.resultsCell";
+    }
+    
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *facilitiesIdentifier = @"facilitiesCell";
-    static NSString *searchIdentifier = @"searchCell"; 
+    NSUInteger section = [self sectionForTableViewSection:indexPath.section];
     
-    if (tableView == self.tableView) {
-        UITableViewCell *cell = nil;
-        cell = [tableView dequeueReusableCellWithIdentifier:facilitiesIdentifier];
-        
-        if (cell == nil) {
-            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                           reuseIdentifier:facilitiesIdentifier] autorelease];
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        }
-        
-        [self configureMainTableCell:cell 
-                        forIndexPath:indexPath];
-        return cell;
-    } else if (tableView == self.searchDisplayController.searchResultsTableView) {
-        HighlightTableViewCell *hlCell = nil;
-        hlCell = (HighlightTableViewCell*)[tableView dequeueReusableCellWithIdentifier:searchIdentifier];
-        
-        if (hlCell == nil) {
-            hlCell = [[[HighlightTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                                    reuseIdentifier:searchIdentifier] autorelease];
+    NSString *cellIdentifier = [self cellIdentifierForSection:section];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    switch(section)
+    {
+        case LocationSearchSectionAsEntered:
+        {
+            if (cell == nil)
+            {
+                cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                               reuseIdentifier:cellIdentifier] autorelease];
+            }
             
-            hlCell.autoresizesSubviews = YES;
-            hlCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.textLabel.text = [NSString stringWithFormat:@"Search for '%@'", self.searchString];
+            break;
         }
-        
-        if (indexPath.row == 0) {
-            hlCell.highlightLabel.searchString = nil;
-            hlCell.highlightLabel.text = [NSString stringWithFormat:@"Use \"%@\"",self.searchString];
-        } else {
-            NSIndexPath *path = [NSIndexPath indexPathForRow:(indexPath.row-1)
-                                                   inSection:indexPath.section];
-            [self configureSearchCell:hlCell
-                         forIndexPath:path];
+            
+        case LocationSearchSectionRecent:
+        {
+            if (cell == nil)
+            {
+                cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                               reuseIdentifier:cellIdentifier] autorelease];
+            }
+            
+            cell.textLabel.text = [self.filteredRecentSearches objectAtIndex:indexPath.row];
+            break;
         }
-        
-        
-        return hlCell;
-    } else {
-        return nil;
+            
+        case LocationSearchSectionQuery:
+        {
+            if (cell == nil)
+            {
+                cell = [[[HighlightTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                      reuseIdentifier:cellIdentifier] autorelease];
+            }
+            
+            HighlightTableViewCell *hlCell = (HighlightTableViewCell*)cell;
+            NSString *labelString = hlCell.highlightLabel.searchString;
+            if ([labelString isEqualToString:self.searchString] == NO)
+            {
+                hlCell.highlightLabel.searchString = self.searchString;
+            }
+            
+            NSDictionary *loc = [self.queryResults objectAtIndex:indexPath.row];
+            hlCell.highlightLabel.text = [loc objectForKey:FacilitiesSearchResultDisplayStringKey];
+            
+            break;
+        }
     }
+    
+    return cell;
 }
 
 #pragma mark - UISearchBarDelegate
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-    self.trimmedString = [searchText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    if (![self.searchString isEqualToString:self.trimmedString]) {
-        self.searchString = ([self.trimmedString length] > 0) ? self.trimmedString : nil;
-        self.filteredData = nil;
+    NSString *trimmedString = [searchText stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if (![self.searchString isEqualToString:trimmedString]) {
+        self.searchString = ([trimmedString length] > 0) ? trimmedString : nil;
+        self.queryResults = nil;
+        self.filteredRecentSearches = nil;
     }
 }
 
@@ -167,16 +433,20 @@ enum {
 }
 
 #pragma mark - UISearchDisplayControllerDelegate
-// Make sure tapping the status bar always scrolls to the top of the active table
-- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView {
-    self.tableView.scrollsToTop = NO;
+- (void)searchDisplayController:(UISearchDisplayController *)controller didShowSearchResultsTableView:(UITableView *)tableView
+{
     tableView.scrollsToTop = YES;
 }
 
-- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView {
-    // using willUnload because willHide strangely doesn't get called when the "Cancel" button is clicked
-    tableView.scrollsToTop = NO;
-    self.tableView.scrollsToTop = YES;
+// Make sure tapping the status bar always scrolls to the top of the active table
+- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView
+{
+    
+}
+
+- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView
+{
+    
 }
 
 @end
