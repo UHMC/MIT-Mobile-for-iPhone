e878bd1a68c9a7080ac267d2a179a5f415cb37de
Blake Skinner <bskinner@mit.edu>
2012-04-03 10:28:42 -0400

    Use autoreleased ivars instead of manually releasing everything. Test to see if this helps the weird crashes we are getting in this code.

--- Common/MITLogging.m
+++ Common/MITLogging.m
@@ -10,14 +10,12 @@ void MyLog(const char *level, const char *filepath, int line, NSString *format, 
     NSString *formattedString = [[NSString alloc] initWithFormat:format
                                                        arguments:args];
     va_end(args);
-    NSString *pathString = [[NSString alloc] initWithCString:filepath encoding:NSUTF8StringEncoding];
+    NSString *pathString = [NSString stringWithUTF8String:filepath];
     NSString *outputFormat = (level == NULL) ? @"[%1$@:%2$d] %3$@\n" : @"%4$s [%1$@:%2$d] %3$@\n";
-    NSString *outputString = [[NSString alloc] initWithFormat:outputFormat, [pathString lastPathComponent], line, formattedString, level];
+    NSString *outputString = [NSString stringWithFormat:outputFormat, [pathString lastPathComponent], line, formattedString, level];
     
     [(NSFileHandle *)[NSFileHandle fileHandleWithStandardOutput] 
-     writeData:[outputString dataUsingEncoding: NSUTF8StringEncoding]];
+     writeData:[outputString dataUsingEncoding:NSUTF8StringEncoding]];
     
-    [pathString release];
     [formattedString release];
-    [outputString release];
 }
