b89b9470f48c5dbf6d7daf53e972157b796555d9
Blake Skinner <bskinner@mit.edu>
2012-04-19 10:25:58 -0400

    Move the list of managed models into a property instead of having it inlined with the code. Should make future additions a bit easier.

--- Common/Core Data/CoreDataManager.h
+++ Common/Core Data/CoreDataManager.h
@@ -11,8 +11,8 @@
 @property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
 @property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
 @property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
 @property (nonatomic, readonly) NSString *applicationDocumentsDirectory;
+@property (nonatomic, readonly) NSSet* modelNames;
 
 +(id)coreDataManager;
 
