82800f06e0e5837e7bb4c738938778faab9905b5
Blake Skinner <bskinner@mit.edu>
2012-05-17 15:44:16 -0400

    Implement isEqual: and -hash

--- Modules/ArcGIS Map/Model/MGSMapCoordinate.m
+++ Modules/ArcGIS Map/Model/MGSMapCoordinate.m
@@ -9,96 +9,106 @@
 
 @dynamic x, y;
 
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
+- (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super init];
-    
-    if (self)
-    {
+
+    if (self) {
         self.longitude = [aDecoder decodeIntegerForKey:@"edu.mit.mobile.MITMapCoordinate.longitude"];
         self.latitude = [aDecoder decodeIntegerForKey:@"edu.mit.mobile.MITMapCoordinate.latitude"];
     }
-    
+
     return self;
 }
 
 
-- (id)initWithLocation:(CLLocationCoordinate2D)location
-{
+- (id)initWithLocation:(CLLocationCoordinate2D)location {
     return [self initWithX:location.longitude
                          y:location.latitude];
 }
 
 
-
-- (id)initWithX:(double)x y:(double)y
-{
+- (id)initWithX:(double)x y:(double)y {
     return [self initWithLongitude:x
                           latitude:y];
 }
-            
-- (id)initWithLongitude:(double)longitude latitude:(double)latitude
-{
+
+- (id)initWithLongitude:(double)longitude latitude:(double)latitude {
     self = [super init];
-    if (self)
-    {
+    if (self) {
         self.longitude = longitude;
         self.latitude = latitude;
     }
-    
+
     return self;
 }
 
-- (BOOL)isEqual:(id)object
-{
-    if ([object isKindOfClass:[self class]])
-    {
-        MGSMapCoordinate *coord = (MGSMapCoordinate*)object;
-        return ((self.longitude == coord.longitude) &&
-                (self.latitude == coord.latitude));
-    }
-    
-    return NO;
-}
-
-- (id)copyWithZone:(NSZone *)zone
-{
+- (id)copyWithZone:(NSZone *)zone {
     return [[MGSMapCoordinate allocWithZone:zone] initWithLongitude:self.longitude
                                                            latitude:self.latitude];
 }
 
-- (void)encodeWithCoder:(NSCoder *)aCoder
-{
-    [aCoder encodeInteger:self.longitude
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeDouble:self.longitude
                    forKey:@"edu.mit.mobile.MITMapCoordinate.longitude"];
-    [aCoder encodeInteger:self.latitude
+    [aCoder encodeDouble:self.latitude
                    forKey:@"edu.mit.mobile.MITMapCoordinate.latitude"];
 }
 
-- (CLLocationCoordinate2D)wgs84Location
-{
+- (CLLocationCoordinate2D)wgs84Location {
     return CLLocationCoordinate2DMake(self.latitude,
                                       self.longitude);
 }
 
 #pragma mark - Properties
-- (void)setX:(double)x
-{
+- (void)setX:(double)x {
     self.longitude = x;
 }
 
-- (double)x
-{
+- (double)x {
     return self.longitude;
 }
 
-- (void)setY:(double)y
-{
+- (void)setY:(double)y {
     self.latitude = y;
 }
 
-- (double)y
-{
+- (double)y {
     return self.latitude;
+}
+
+#pragma mark - Overridden Methods
+- (BOOL)isEqual:(id)object {
+    if (object == self) {
+        return YES;
+    }
+    else if ([object isKindOfClass:[self class]]) {
+        return [self isEqualToCoordinate:(MGSMapCoordinate *) object];
+    }
+    else {
+        return [super isEqual:object];
+    }
+}
+
+- (BOOL)isEqualToCoordinate:(MGSMapCoordinate *)mapAnnotation {
+    if (mapAnnotation == self) {
+        return YES;
+    }
+    else {
+        return ((self.longitude == mapAnnotation.longitude) &&
+                (self.latitude == mapAnnotation.latitude));
+    }
+}
+
+- (NSUInteger)hash {
+    NSUInteger hash = 0;
+    double ip, fp;
+
+    fp = modf(self.longitude, &ip);
+    hash = (NSUInteger)(fabs(fp) * ip);
+
+    fp = modf(self.latitude, &ip);
+    hash ^= (NSUInteger)(fabs(fp) * ip);
+
+    return hash;
 }
 @end
